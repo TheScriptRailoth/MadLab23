b210968df3d81f0e3b580c5e9649e37c464a4c53
TheScriptRailoth <am3718440@gmail.com>
2023-05-17 02:53:07 +0530

    MadLab23: Lab Exercises
    Signed-off-by: TheScriptRailoth <am3718440@gmail.com>

---
+++ LE1/LE1_Prob3.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String args[])
+    {
+        int a=Integer.parseInt(args[0]);
+        for(int i=1;i<11;i++)
+        {
+            System.out.println(a+"x"+i+"= "+a*i);
+        }
+    }
+}
